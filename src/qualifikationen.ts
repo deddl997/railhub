aff9174a9c9f0467f9e7551f09b6392b6540f1ae
deddl997 <ssedlmayr@gmx.de>
2026-09-02 15:29:29 +0000

    Auslastungs-Heatmap nach Lok-Typ fuer Lokfuehrer aufgeschluesselt

---
+++ src/qualifikationen.ts
@@ -0,0 +1,9 @@
+export const KATEGORIEN = [
+  'Lokführer',
+  'Dienstleister',
+  'Wagenmeister',
+  'Disposition',
+  'Betriebsleitung',
+]
+
+export const LOK_TYPEN = ['DE 18', '185', '193', 'V90', 'V60', 'G1206']
